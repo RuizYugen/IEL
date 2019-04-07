af94490fb2cf142d78cfb1b74143089b0dfa36f8
ElizabethGlz <elizabeth.10.xc@gmail.com>
2019-04-07 12:51:20 -0500

    Avance estadísticas.

---
+++ IEL/IEL/Vistas/JS/Estadisticas.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    var usuario = "Jose1";
+  
+    IEL.Servicios.wsUsuario.getUsuarioByID(usuario, cargarDatos);
+
+});
+var datosObtenidos;
+function cargarDatos(resultado) {
+    
+    datosObtenidos = JSON.parse(resultado);
+   var nombre = datosObtenidos.Nombre+" "+datosObtenidos.ApellidoPaterno+" "+datosObtenidos.ApellidoMaterno;
+    document.getElementById("txtNombreAlumno").innerHTML = nombre;
+}
