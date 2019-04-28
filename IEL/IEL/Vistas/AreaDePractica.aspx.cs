16225d641b216958e9bdb3c2eaba85794ad0b6ed
RuizYugen <s15120040@alumnos.itsur.edu.mx>
2019-04-28 00:24:03 -0500

    AreaDePractca auto genera preguntas

--- IEL/IEL/Vistas/AreaDePractica.aspx.cs
+++ IEL/IEL/Vistas/AreaDePractica.aspx.cs
@@ -13,12 +13,21 @@ namespace IEL.Vistas
     {
         private int tema;
         private string usuario;
+        private Tematica[] preguntas = new Tematica[10];
         public class Tematica
         {
             public string Oracion { get; set; }
             public string Buena { get; set; }
             public string mala1 { get; set; }
             public string mala2 { get; set; }
+            public string Respuesta { get; set; }
+            override
+            public string ToString()
+            {
+                string cadena;
+                cadena = Oracion + "#" + Buena+"#"+mala1+"#"+mala2+"#"+Respuesta;
+                return cadena;
+            }
         }
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -27,74 +36,186 @@ namespace IEL.Vistas
                 Response.Redirect("Login.aspx");
             }else
             {
-                usuario = Session["usuario"].ToString();
-                //Cambiar por la session
-                tema = 1;
-                GramaticaDAO dao = new GramaticaDAO();
-                List<Gramatica> lista = dao.getByTema(tema);
-                Gramatica gramatica = lista[new Random().Next(0, lista.Count)];
-                string[] arr = gramatica.Formula.Split('_');
-                //10 preguntas
-                Tematica[] preguntas = new Tematica[10];
-                SujetoDAO sujetoDao = new SujetoDAO();
-                List<Sujeto> sujetos= sujetoDao.getAll();
-                VerboDAO verboDao = new VerboDAO();
-                List<Verbo> verbos = verboDao.getAll();
-                int no,nov;
-                for (int i=0; i<10; i++)
+                if (Session["flag"]==null)
                 {
-                    Tematica tmt = new Tematica();
-                   
-                    no= new Random().Next(0, sujetos.Count);
-                    no = 0;
-                    nov = new Random().Next(0, verbos.Count);
-                    for (int j=0; j<arr.Length; j++)
+                    Session["posicion"] = 0;
+                    usuario = Session["usuario"].ToString();
+                    //Cambiar por la session
+                    tema = 1;
+                    GramaticaDAO dao = new GramaticaDAO();
+                    List<Gramatica> lista = dao.getByTema(tema);
+                    Gramatica gramatica = lista[new Random().Next(0, lista.Count)];
+                    string[] arr = gramatica.Formula.Split('_');
+                    //10 preguntas
+
+                    SujetoDAO sujetoDao = new SujetoDAO();
+                    List<Sujeto> sujetos = sujetoDao.getAll();
+                    VerboDAO verboDao = new VerboDAO();
+                    List<Verbo> verbos = verboDao.getAll();
+                    int no, nov;
+                    for (int i = 0; i < 10; i++)
                     {
-                        switch (arr[j])
+                        Tematica tmt = new Tematica();
+
+                        no = new Random().Next(0, sujetos.Count);                       
+                        nov = new Random().Next(0, verbos.Count-10);
+                        nov += i;
+                        for (int j = 0; j < arr.Length; j++)
                         {
-                            case "sujeto":
-                                tmt.Oracion = sujetos[no].sujeto+" ";
-                                break;
-                            case "tobe":
-                                tmt.Oracion += "   ";
-                                tmt.Buena = sujetos[no].ser;
-                                if (tmt.Buena.Equals("am")){
-                                    tmt.mala1 = "are";
-                                    tmt.mala2 = "is";
-                                }else if (tmt.Buena.Equals("is"))
-                                {
-                                    tmt.mala1 = "are";
-                                    tmt.mala2 = "am";
-                                }else
-                                {
-                                    tmt.mala1 = "am";
-                                    tmt.mala2 = "is";
-                                }
-                                break;
-                            case "verbopresentesimple":
-                                if (gramatica.TerceraPersona)
-                                {
-                                    tmt.Oracion += verbos[nov].VerboPresenteSimple + "s ";
-                                }else
-                                {
-                                    tmt.Oracion += verbos[nov].VerboPresenteSimple + " ";
-                                }
-                                break;
-                            case "not":
-                                tmt.Oracion += "not ";
-                                break;
-                            case "complemento":
-                                tmt.Oracion += "in the school";
-                                break;
-                            default:
-                                break;
-                        }                        
+                            switch (arr[j])
+                            {
+                                case "sujeto":
+                                    tmt.Oracion = sujetos[no].sujeto + " ";
+                                    break;
+                                case "tobe":
+                                    tmt.Oracion += "   ";
+                                    tmt.Buena = sujetos[no].ser;
+                                    if (tmt.Buena.Equals("am"))
+                                    {
+                                        tmt.mala1 = "are";
+                                        tmt.mala2 = "is";
+                                    }
+                                    else if (tmt.Buena.Equals("is"))
+                                    {
+                                        tmt.mala1 = "are";
+                                        tmt.mala2 = "am";
+                                    }
+                                    else
+                                    {
+                                        tmt.mala1 = "am";
+                                        tmt.mala2 = "is";
+                                    }
+                                    break;
+                                case "verbopresentesimple":
+                                    if (gramatica.TerceraPersona)
+                                    {
+                                        tmt.Oracion += verbos[nov].VerboPresenteSimple + "s ";
+                                    }
+                                    else
+                                    {
+                                        tmt.Oracion += verbos[nov].VerboPresenteSimple + " ";
+                                    }
+                                    break;
+                                case "not":
+                                    tmt.Oracion += "not ";
+                                    break;
+                                case "complemento":
+                                    tmt.Oracion += "in the school";
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        preguntas[i] = tmt;
                     }
-                    preguntas[i] = tmt;
+                    string cadena="";
+                    for(int i=0; i<10; i++)
+                    {
+                        cadena += preguntas[i].ToString();
+                        if (i < 9)
+                        {
+                            cadena += "$";
+                        }
+                    }
+                    Session["flag"] = cadena;
+                    txtPregunta.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Oracion;
+                    int mun = new Random().Next(0, 3);
+                    if (mun == 0)
+                    {
+                        rdbA.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Buena;
+                        rdbB.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala1;
+                        rdbC.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala2;
+                    }
+                    else if (mun == 1)
+                    {
+                        rdbA.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala1;
+                        rdbB.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Buena;
+                        rdbC.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala2;
+                    }
+                    else if (mun == 2)
+                    {
+                        rdbA.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala1;
+                        rdbB.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala2;
+                        rdbC.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Buena;
+
+                    }
                 }
-                txtPregunta.Text = preguntas[0].Oracion;
+                else
+                {
+                    string[] des = Session["flag"].ToString().Split('$');
+                    for(int i=0; i<des.Length; i++)
+                    {
+                        string[] der = des[i].Split('#');
+                        preguntas[i] = new Tematica() { Oracion=der[0],Buena=der[1],mala1=der[2],mala2=der[3],Respuesta=der[4]};
+                    }
+                }
             }
             
         }
+
+        protected void rdbB_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    
+        protected void btnAnterior_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(Session["posicion"].ToString()) > 0)
+            {
+                Session["posicion"] = Convert.ToInt32(Session["posicion"].ToString())-1;
+                txtPregunta.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Oracion;
+                int mun = new Random().Next(0, 3);
+                if (mun == 0)
+                {
+                    rdbA.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Buena;
+                    rdbB.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala1;
+                    rdbC.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala2;
+                }
+                else if (mun == 1)
+                {
+                    rdbA.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala1;
+                    rdbB.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Buena;
+                    rdbC.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala2;
+                }
+                else if (mun == 2)
+                {
+                    rdbA.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala1;
+                    rdbB.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala2;
+                    rdbC.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Buena;
+
+                }
+            }
+        }
+
+        protected void btnSiguiente_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(Session["posicion"].ToString()) < 9)
+            {
+                Session["posicion"] = Convert.ToInt32(Session["posicion"].ToString()) + 1;
+                txtPregunta.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Oracion;
+                int mun = new Random().Next(0, 3);
+                if (mun == 0)
+                {
+                    rdbA.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Buena;
+                    rdbB.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala1;
+                    rdbC.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala2;
+                }
+                else if (mun == 1)
+                {
+                    rdbA.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala1;
+                    rdbB.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Buena;
+                    rdbC.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala2;
+                }
+                else if (mun == 2)
+                {
+                    rdbA.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala1;
+                    rdbB.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].mala2;
+                    rdbC.Text = preguntas[Convert.ToInt32(Session["posicion"].ToString())].Buena;
+
+                }
+            }
+        }
+
     }
 }
