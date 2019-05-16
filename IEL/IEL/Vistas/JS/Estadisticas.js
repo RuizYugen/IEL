891b3496790d5c489096461ad05d74b3d51c2bd0
ElizabethGlz <elizabeth.10.xc@gmail.com>
2019-05-15 23:31:30 -0500

    Sin cambios.

--- IEL/IEL/Vistas/JS/Estadisticas.js
+++ IEL/IEL/Vistas/JS/Estadisticas.js
@@ -1,58 +1,29 @@
 ﻿var usuario;
+//Llama low métodos de los web services necesarios para obtener los datos que serán cargados en la página.
 $(document).ready(function () {
-    //var usuario = "Jose1";
     usuario = $('#logear').text();
     IEL.Servicios.wsUsuario.getUsuarioByID(usuario, cargarDatos);
     IEL.Servicios.wsPractica.getTotalPresenteByUser(usuario, cargarEstadisticas);
     IEL.Servicios.wsPractica.getTotalPasadoByUser(usuario, estadisticasPasado);
     IEL.Servicios.wsPractica.getTotalVerbosByUser(usuario, estadisticasVerbos);
-    IEL.Servicios.wsPractica.getTotalVocabularioByUser(usuario, estadisticasVocabulario);
-   // IEL.Servicios.wsPreguntaContestadaPorUsuario.getPreguntasCorrectasPresenteSimple(usuario,cargarEstadisticas);
-    //IEL.Servicios.wsPreguntaPorTema.getPreguntasPorTema(2, preguntasPasado);
-    //IEL.Servicios.wsPreguntaContestadaPorUsuario.getPreguntasCorrectasPasadoSimple(usuario, estadisticasPasado);
-    //IEL.Servicios.wsPreguntaPorTema.getPreguntasPorTema(3, preguntasVerbos);
-    //IEL.Servicios.wsPreguntaContestadaPorUsuario.getPreguntasCorrectasVerbos(usuario, estadisticasVerbos);
-    //IEL.Servicios.wsPreguntaPorTema.getPreguntasPorTema(4, preguntasVocabulario);
-    //IEL.Servicios.wsPreguntaContestadaPorUsuario.getPreguntasCorrectasVocabulario(usuario, estadisticasVocabulario);
-    
+    IEL.Servicios.wsPractica.getTotalVocabularioByUser(usuario, estadisticasVocabulario); 
 });
 
 
-var datosObtenidos;
+var datosObtenidos;//Objeto de usuario correspondiente al usuario que desea consultar las estádistias.
 
-var numPreguntasTotalPresente=0;
-var preguntasCorrectasPresente;
-var numPreguntasTotalPasado = 0;
-var preguntasCorrectasPasado;
-var numPreguntasTotalVerbos = 0;
-var preguntasCorrectasVerbos;
-var numPreguntasTotalVocabulario = 0;
-var preguntasCorrectasVocabulario;
-function preguntasPresente(resultado) {
-   var preguntasTotal = JSON.parse(resultado);
-   numPreguntasTotalPresente = preguntasTotal.length;
-   
-}
-function preguntasPasado(resultado) {
-    var preguntasTotal = JSON.parse(resultado);
-    numPreguntasTotalPasado = preguntasTotal.length;
-
-}
-function preguntasVerbos(resultado) {
-    var preguntasTotal = JSON.parse(resultado);
-    numPreguntasTotalVerbos = preguntasTotal.length;
-}
-
-function preguntasVocabulario(resultado) {
-    var preguntasTotal = JSON.parse(resultado);
-    numPreguntasTotalVocabulario = preguntasTotal.length;
-}
+//Carga el nombre del usuario en la página. Recibe un JSON con el objeto usuario, y lo convierte a objeto.
+//Se acceden a los atributos del objeto para poder mostrar el nombre completo del usuario.
 function cargarDatos(resultado) {
     
     datosObtenidos = JSON.parse(resultado);
    var nombre = datosObtenidos.Nombre+" "+datosObtenidos.ApellidoPaterno+" "+datosObtenidos.ApellidoMaterno;
     document.getElementById("txtNombreAlumno").innerHTML = nombre;
 }
+//Carga el progreso total del presente simple.
+//Recibe la cantidad de preguntas contestadas del tema presente simple, este resultado es multiplicado por 10 para obtener el porcentaje.
+//Se asigna el porcentaje obtenido al elemento correspondiente para ser visualizado en la página.
+//El resultado obtenido, además es guardado en la variable global avanceps que será utilizada para calcular el progreso total.
 var avanceps;
 function cargarEstadisticas(resultado) {
     var contestadas = JSON.parse(resultado);
@@ -60,6 +31,10 @@ function cargarEstadisticas(resultado) {
     avanceps = avancePS;
     document.getElementById("txtSimplePresent").innerHTML = "Simple present: " + avancePS + "%";
 }
+//Carga el progreso total del pasado simple.
+//Recibe la cantidad de preguntas contestadas del tema pasado simple, este resultado es multiplicado por 10 para obtener el porcentaje.
+//Se asigna el porcentaje obtenido al elemento correspondiente para ser visualizado en la página.
+//El resultado obtenido, además es guardado en la variable global avancePasado que será utilizada para calcular el progreso total.
 var avancePasado;
 function estadisticasPasado(resultado) {
     var contestadas = JSON.parse(resultado);
@@ -67,6 +42,10 @@ function estadisticasPasado(resultado) {
     avancePasado = avancePS;
     document.getElementById("txtSimplePast").innerHTML = "Simple past: " + avancePS + "%";
 }
+//Carga el progreso total del tema verbos.
+//Recibe la cantidad de preguntas contestadas del tema verbos, este resultado es multiplicado por 10 para obtener el porcentaje.
+//Se asigna el porcentaje obtenido al elemento correspondiente para ser visualizado en la página.
+//El resultado obtenido, además es guardado en la variable global avanceVerbos que será utilizada para calcular el progreso total.
 var avanceVerbos;
 function estadisticasVerbos(resultado) {
     var contestadas = JSON.parse(resultado);
@@ -74,6 +53,11 @@ function estadisticasVerbos(resultado) {
     avanceVerbos = avancePS;
     document.getElementById("txtVerbs").innerHTML = "Verbs: " + avancePS + "%";
 }
+//Carga el progreso total del tema vocabulario.
+//Recibe la cantidad de preguntas contestadas del tema vocabulario, este resultado es multiplicado por 10 para obtener el porcentaje.
+//Se asigna el porcentaje obtenido al elemento correspondiente para ser visualizado en la página.
+//El resultado obtenido, además es guardado en la variable global avanceVc que será utilizada para calcular el progreso total.
+//Se manda llamar el método cargarAvance() para que puedan ser visualizadas las barras de progreso.
 var avanceVc;
 function estadisticasVocabulario(resultado) {
     var contestadas = JSON.parse(resultado);
@@ -83,8 +67,10 @@ function estadisticasVocabulario(resultado) {
 
     cargarAvance();
 }
+//Se obtiene el progreso total calculando el promedio de todos los temas, para esto se hace uso de las variables avanceps,
+//avancePasado, avanceVerbos y avanceVc, dónde se almacenó el progreso total de caa uno de los temas.
+//Se le asigna el valor a las barras de progreso correspondientes a cada tema, y a la barra del progreso total. 
 function cargarAvance() {
-    //debugger;
     var avance = (avanceps + avancePasado + avanceVerbos + avanceVc) / 4;
     document.getElementById("barraPresente").value = avanceps;
     document.getElementById("barraPasado").value = avancePasado;
